6722ed3a5b040ec3cf4227dcd91a8960c6c9d874
natisalvarez <naatisalvarez@gmail.com>
2023-08-18 20:14:32 +0200

    probando front

--- back/src/app.js
+++ back/src/app.js
@@ -23,7 +23,7 @@ server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'bonitaandlovely-front-production.up.railway.app' /* '*' */);
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');

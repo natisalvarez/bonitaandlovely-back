6cf4152dbc3ea5b01b60ebdae4b978ec9615375a
natisalvarez <naatisalvarez@gmail.com>
2023-08-20 15:21:25 +0200

    cambios en app

--- back/src/app.js
+++ back/src/app.js
@@ -1,3 +1,4 @@
+const express = require('express');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
@@ -10,44 +11,6 @@ const { MERCADO_PAGO_ACCESS_TOKEN, MERCADO_PAGO_KEY } = process.env
 mercadopago.configure({
   access_token: MERCADO_PAGO_ACCESS_TOKEN,
 });
-require('./db.js');
-
-const server = express();
-
-server.name = 'API';
-
-server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
-server.use(bodyParser.json({ limit: '50mb' }));
-server.use(cookieParser());
-server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  next();
-});
-server.use(helmet()); // Utilizamos el paquete 'helmet' para agregar capas de seguridad
-
-// Colocamos el middleware de configuración CORS antes del middleware 'morgan'
-server.use(morgan('dev'));
-
-server.use('/', routes);
-
-// Agregamos manejadores específicos para distintos tipos de errores
-server.use((err, req, res, next) => {
-  const status = err.status || 500;
-  const message = err.message || err;
-  console.error(err);
-  res.status(status).send(message);
-});
-
-// Implementamos un manejo de errores más robusto, registrando los errores en un archivo de registro
-server.use((err, req, res, next) => {
-  // Aquí podrías agregar el código para guardar los errores en un archivo de registro o en una base de datos de registro de errores
-  console.error('Error:', err.message);
-  res.status(500).send('Internal Server Error');
-});
-
 require('./db.js');
 
 const server = express();

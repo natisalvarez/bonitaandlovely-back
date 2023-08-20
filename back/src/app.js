4318d508f99105efab7f424e9a8ea892072d391c
natisalvarez <naatisalvarez@gmail.com>
2023-08-20 15:38:23 +0200

    cambios en la ruta de index html

--- back/src/app.js
+++ back/src/app.js
@@ -51,7 +51,7 @@ server.use((err, req, res, next) => {
 
 //Mercado pago:
 server.get("/", function (req, res) {
-  const filePath = path.resolve(__dirname, "..", "public", "Frond", "index.html"); 
+  const filePath = path.resolve(__dirname, "..", "Frond", "public", "index.html"); 
   res.sendFile(filePath);
 });
 
